56368cd1f00c9f9c9c83f994991a97d3cc947cd8
Clement CATON <clement.caton@algosup.com>
2023-04-11 21:50:56 +0200

    unpolished game without content

---
+++ TheCardKingdom/Source/TheCardKingdom/TheCardKingdomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheCardKingdomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THECARDKINGDOM_API ATheCardKingdomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
