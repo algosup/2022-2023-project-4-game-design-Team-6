c2febbb6af19c61ff89b983e73de04493f26e22e
Aurélien <aurelien.fernandez@algosup.com>
2023-04-12 11:36:54 +0200

    Game update
    imported every functions from save branch to the last game version, it includes saving, dynamic UI, main menu, pause menu and basic reset system

---
+++ TheCardKingdom/Intermediate/Build/Win64/TheCardKingdomEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.34.31937
+#include "E:/Projets/gameproj2/TheCardKingdom/Intermediate/Build/Win64/TheCardKingdomEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ TheCardKingdom/Intermediate/Build/Win64/UE4Editor/Inc/TheCardKingdom/TheCardKingdomGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef THECARDKINGDOM_TheCardKingdomGameModeBase_generated_h
+#error "TheCardKingdomGameModeBase.generated.h already included, missing '#pragma once' in TheCardKingdomGameModeBase.h"
+#endif
+#define THECARDKINGDOM_TheCardKingdomGameModeBase_generated_h
+
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_SPARSE_DATA
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_RPC_WRAPPERS
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATheCardKingdomGameModeBase(); \
+	friend struct Z_Construct_UClass_ATheCardKingdomGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATheCardKingdomGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TheCardKingdom"), NO_API) \
+	DECLARE_SERIALIZER(ATheCardKingdomGameModeBase)
+
+
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATheCardKingdomGameModeBase(); \
+	friend struct Z_Construct_UClass_ATheCardKingdomGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATheCardKingdomGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TheCardKingdom"), NO_API) \
+	DECLARE_SERIALIZER(ATheCardKingdomGameModeBase)
+
+
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATheCardKingdomGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATheCardKingdomGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATheCardKingdomGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATheCardKingdomGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATheCardKingdomGameModeBase(ATheCardKingdomGameModeBase&&); \
+	NO_API ATheCardKingdomGameModeBase(const ATheCardKingdomGameModeBase&); \
+public:
+
+
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATheCardKingdomGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATheCardKingdomGameModeBase(ATheCardKingdomGameModeBase&&); \
+	NO_API ATheCardKingdomGameModeBase(const ATheCardKingdomGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATheCardKingdomGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATheCardKingdomGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATheCardKingdomGameModeBase)
+
+
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_12_PROLOG
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_SPARSE_DATA \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_RPC_WRAPPERS \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_INCLASS \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_SPARSE_DATA \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> THECARDKINGDOM_API UClass* StaticClass<class ATheCardKingdomGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TheCardKingdom_Source_TheCardKingdom_TheCardKingdomGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
